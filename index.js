4879a480ecc395627f565439470be0490832585c
Gesthalt <nsakunov43@gmail.com>
2023-08-10 05:14:29 -0400

    Changed index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-
-let computerSelection;
-let playerSelection;
-let playerScore = 0;
-let computerScore = 0;
 game();
 
 //This function extracts first letter from playerSelection and stores it in str1. Str2 stores the rest string. It then joins the modified str1 and str2
@@ -12,8 +7,7 @@ function stringCompare(playerSelection)
     let str2 = playerSelection.slice(1);
     str1 = str1.toUpperCase();
     str2 = str2.toLowerCase();
-    playerSelection = str1.concat(str2);
-    return playerSelection;
+    return str1.concat(str2);
 }
     
 
@@ -50,52 +44,71 @@ function singleRound(computerSelection, playerSelection)
         return "You win! Rock beats Scissors";
     }
 
-    if(playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors")
+    if(playerSelection === computerSelection)
     {
-        return "You illiterate dumbass!";
+        return "Tie";
     }
 }
 
-//Generates a random number between 1 - 3 
+
 function getComputerChoice()
 {
     let randomChoice = randomNumber();
 
-    if(randomChoice == 1)
+    switch(randomChoice)
     {
-        return "Rock";
-    }else if(randomChoice == 2)
-    {
-        return "Paper";
-    }else if(randomChoice == 3)
-    {
-        return "Scissors";
+        case 1:
+            randomChoice = "Rock";
+            return "Rock"
+            break;
+        case 2:
+            randomChoice = "Paper";
+            return "Paper"
+            break;
+        case 3:
+            randomChoice = "Scissors";
+            return "Scissors"
+            break;
     }
 }
 
-
+//main function
 function game()
 {
-    
+    let computerSelection;
+    let playerSelection;
+    let playerScore = 0;
+    let computerScore = 0;
+
     for(let i = 1; i <= 5; i++)
     {
         computerSelection = getComputerChoice();
         console.log("This is computer's choice:", computerSelection); //a little cheating
+        
         playerSelection = stringCompare(prompt("Enter your choice: "));
-        let x = singleRound(computerSelection, playerSelection);
-        console.log(x);
-        if(x === "You win! Rock beats Scissors" || x === "You win! Scissors cuts Paper" || x === "You win! Paper wraps Rock")
+
+        let roundResult = singleRound(computerSelection, playerSelection);
+        console.log(roundResult);
+
+        if(roundResult === "You win! Rock beats Scissors" || roundResult === "You win! Scissors cuts Paper" || roundResult === "You win! Paper wraps Rock")
         {
             ++playerScore;
+            console.log("Your wins: ", playerScore);
+            console.log("Computer wins: ", computerScore);
         }
-        else{
+        else if(roundResult === "Tie"){
+            --i;
+            continue;
+        }else {
             ++computerScore;
+            console.log("Your wins: ", playerScore);
+            console.log("Computer wins: ", computerScore);
         }
-        console.log("Your wins: ", playerScore);
-        console.log("Computer wins: ", computerScore);
+        
     }
 }
 
+//Generates a random number between 1 - 3 
 function randomNumber()
 {
     return Math.floor(Math.random() * 3) + 1;
